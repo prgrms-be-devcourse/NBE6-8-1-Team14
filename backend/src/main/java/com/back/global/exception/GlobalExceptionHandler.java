b7c293337993d8d9d5cfeb4328318489535da026
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2025-07-14 23:02:15 +0900

    fix: import문 추가

--- backend/src/main/java/com/back/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/back/global/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.back.global.common.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 /**
  * 전역 예외 처리 핸들러
