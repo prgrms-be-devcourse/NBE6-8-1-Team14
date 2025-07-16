3181967f8c2ab49cbbc3c5b76d355ed98472919a
Chaeruin <cms3604@naver.com>
2025-07-16 14:24:56 +0900

    style: operation 추가

--- backend/src/main/java/com/back/domain/order/controller/OrderController.java
+++ backend/src/main/java/com/back/domain/order/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.back.domain.order.dto.request.OrderRequestDto;
 import com.back.domain.order.dto.response.OrderResponseDto;
 import com.back.domain.order.service.OrderService;
 import com.back.global.common.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/direct")
+    @Operation(summary = "직접 주문 생성 API", description = "주문을 생성하고, 주문 확인 이메일을 발송합니다.")
     public ResponseEntity<ApiResponse<OrderResponseDto>> createOrder(@RequestBody OrderRequestDto orderRequestDto) {
         OrderResponseDto createdOrder = orderService.createOrder(orderRequestDto);
         orderService.sendOrderConfirmationEmail(orderRequestDto, createdOrder);
@@ -34,6 +36,7 @@ public class OrderController {
     }
 
     @PostMapping("/base-address")
+    @Operation(summary = "기본 배송지 변경 API", description = "회원의 기본 배송지를 변경합니다.")
     public ResponseEntity<ApiResponse<Void>> changeMemberBaseAddress(@RequestBody OrderBaseAddressRequestDto orderBaseAddressRequestDto) {
         orderService.changeMemberBaseAddress(orderBaseAddressRequestDto);
         return ResponseEntity.status(HttpStatus.OK).body(
@@ -42,6 +45,7 @@ public class OrderController {
     }
 
     @GetMapping("/{orderId}")
+    @Operation(summary = "주문 상세 조회 API", description = "주문 ID로 주문의 상세 정보를 조회합니다.")
     public ResponseEntity<ApiResponse<OrderResponseDto>> showOrder(@PathVariable Long orderId) {
         OrderResponseDto orderResponseDto = orderService.showOrder(orderId);
         return ResponseEntity.status(HttpStatus.OK).body(
@@ -50,6 +54,7 @@ public class OrderController {
     }
 
     @DeleteMapping("/{orderId}")
+    @Operation(summary = "주문 취소 API", description = "주문 ID로 주문을 취소합니다.")
     public ResponseEntity<ApiResponse<Void>> cancelOrder(@PathVariable Long orderId) {
         orderService.cancelOrder(orderId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(
