dd65cd2d85faccede71b075d370c48985863f195
Chaeruin <cms3604@naver.com>
2025-07-16 09:39:18 +0900

    feat: 주문 관련 필요한 repository 생성

---
+++ backend/src/main/java/com/back/domain/product/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.back.domain.product.repository;
+
+import com.back.domain.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
