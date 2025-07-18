b68dbdfc2a98f68ed9b594ed53b6bfea5acac1e6
SeokGeunHo <kimc6555@gmail.com>
2025-07-18 16:23:54 +0900

    refactor/be: 상품 CRUD 피드백 반영

--- backend/src/main/java/com/back/domain/product/controller/ProductController.java
+++ backend/src/main/java/com/back/domain/product/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.back.domain.product.controller;
 import com.back.domain.product.dto.ProductDto;
 import com.back.domain.product.dto.ProductRequestDto;
 import com.back.domain.product.dto.ProductUpdateRequestDto;
-import com.back.domain.product.entity.Product;
 import com.back.domain.product.service.ProductService;
 import com.back.global.common.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,7 +11,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,7 +25,6 @@ public class ProductController {
 
     @Operation(summary = "상품 등록", description = "상품 등록 API")
     @PostMapping
-    @Transactional
     public ResponseEntity<ApiResponse<ProductDto>> create(
             @Valid @RequestBody ProductRequestDto reqBody
     ) {
@@ -35,84 +32,84 @@ public class ProductController {
 //        Member actor = rq.getActor();
 
         // 상품 등록
-        Product product = productService.create(
-                reqBody.name(),
-                reqBody.price(),
-                reqBody.description(),
-                reqBody.imagePath()
-        );
+        ProductDto productDto = productService.create(reqBody);
 
         // 성공 응답
         return ResponseEntity.ok(
                 ApiResponse.success(
-                        "%s 상품이 등록되었습니다.".formatted(product.getName()),
-                        new ProductDto(product)
+                        "%s 상품이 등록되었습니다.".formatted(productDto.name()),
+                        productDto
                 )
         );
     }
 
     @Operation(summary = "상품 전체 조회", description = "상품 전체 조회 API")
     @GetMapping
-    @Transactional(readOnly = true)
-    public List<ProductDto> getAllProducts() {
-        List<Product> products = productService.findAll();
+    public ResponseEntity<ApiResponse<List<ProductDto>>> getAllProducts() {
+        List<ProductDto> productDtoList = productService.findAll();
 
         // 성공 응답
-        return products
-                .stream()
-                .map(ProductDto::new) // ProductDto 변환
-                .toList();
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        "상품 전체 조회입니다.",
+                        productDtoList
+                )
+        );
+    }
+
+    @Operation(summary = "상품 단일 조회", description = "상품 단일 조회 API")
+    @GetMapping("/{productId}")
+    public ResponseEntity<ApiResponse<ProductDto>> getProductById(
+            @PathVariable Long productId
+    ) {
+        ProductDto productDto = new ProductDto(productService.findById(productId));
+
+        // 성공 응답
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        "%s 상품을 조회했습니다".formatted(productDto.name()),
+                        productDto
+                )
+        );
     }
 
     @Operation(summary = "상품 수정", description = "상품 수정 API")
-    @PutMapping("/{id}")
-    @Transactional
-    public ResponseEntity<ApiResponse<String>> modify(
-            @PathVariable int id,
+    @PutMapping("/{productId}")
+    public ResponseEntity<ApiResponse<String>> update(
+            @PathVariable Long productId,
             @Valid @RequestBody ProductUpdateRequestDto reqBody
     ) {
         // 권한 체크
 //        Member actor = rq.getActor();
 
-        Product product = productService.findById(id).get();
-
 //        post.checkActorCanModify(actor);
 
-        productService.update(
-                product,
-                reqBody.name(),
-                reqBody.price(),
-                reqBody.description(),
-                reqBody.imagePath()
-        );
+        ProductDto productDto = productService.update(productService.findById(productId), reqBody);
 
         // 성공 응답
         return ResponseEntity.ok(
                 ApiResponse.success(
-                        "%s 번 상품이 수정되었습니다.".formatted(product.getId())
+                        "%s 번 상품이 수정되었습니다.".formatted(productDto.id())
                 )
         );
     }
 
     @Operation(summary = "상품 삭제", description = "상품 삭제 API")
-    @DeleteMapping("/{id}")
-    @Transactional
+    @DeleteMapping("/{productId}")
     public ResponseEntity<ApiResponse<String>> delete(
-            @PathVariable int id
+            @PathVariable Long productId
     ) {
         // 권한 체크
 //        Member actor = rq.getActor();
 
-        Product product = productService.findById(id).get();
-
 //        post.checkActorCanModify(actor);
 
-        productService.delete(product);
+        ProductDto productDto = productService.delete(productService.findById(productId));
 
         // 성공 응답
         return ResponseEntity.ok(
                 ApiResponse.success(
-                        "%s 번 상품이 삭제되었습니다.".formatted(product.getId())
+                        "%s 번 상품이 삭제되었습니다.".formatted(productDto.id())
                 )
         );
     }
