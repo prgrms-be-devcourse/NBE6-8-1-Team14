780d1d8bbbf1e49d000abb3a9792a122857886a8
asowjdan <asowjdan@naver.com>
2025-07-20 08:07:43 +0900

    푸터 추가

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -3,13 +3,17 @@
 import type React from "react"
 import { Header } from "@/components/header/header"
 import { DevRoleSwitcher } from "@/components/dev/auth/DevRoleSwitcher"
+import { Footer } from "@/components/footer/footer"
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
     return (
-        <>
+        <div className="min-h-screen flex flex-col">
             <Header />
-            {children}
+            <main className="flex-1">
+                {children}
+            </main>
             <DevRoleSwitcher />
-        </>
+            <Footer />
+        </div>
     )
 }
