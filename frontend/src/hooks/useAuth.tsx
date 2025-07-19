a62f9a2bfe0d28d5dd0bdef73843d2ea49b371ba
Gooraeng <birdyoon1998@gmail.com>
2025-07-19 15:27:20 +0900

    feat: useAuth 추가

---
+++ frontend/src/hooks/useAuth.tsx
@@ -0,0 +1,76 @@
+import {useEffect, useState, createContext, use} from "react";
+import client from "@/lib/backend/client";
+import { components } from "@/lib/backend/api/schema";
+
+type MemberDto = components["schemas"]["MemberDto"]
+
+export default function useAuth() {
+
+    const [loginMember, setLoginMember] = useState<MemberDto | null>(null)
+    const isLogin = loginMember !== null;
+    const isAdmin = isLogin && loginMember.role === "ADMIN";
+
+    useEffect(() => {
+        client.GET("/api/auth/me").then((res) => {
+            if (res.error) return;
+
+            setLoginMember(res);
+        })
+    }, [])
+
+    const clearLoginMember = () => {
+        setLoginMember(null);
+    }
+
+    const logout = (onSuccess: () => void) => {
+        client.DELETE("api/auth/logout").then(res => {
+            if (res.error) {
+                // 예시
+                alert(res.error.message);
+                return;
+            }
+
+            clearLoginMember();
+
+            onSuccess();
+        })
+    }
+
+    const baseRs = {
+        logout,
+        setLoginMember,
+        isAdmin,
+    };
+
+    if (isLogin) {
+        return {
+            isLogin: true,
+            loginMember,
+            ...baseRs
+        } as const;
+    }
+
+    return {
+        isLogin: false,
+        loginMember: null,
+        ...baseRs,
+    } as const;
+}
+
+const UserContext = createContext<ReturnType<typeof useAuth> | null>(null);
+
+export function AuthProvider({children}: { children: React.ReactNode }) {
+    const authState = useAuth();
+
+    return (
+        <UserContext value={authState}>
+            {children}
+        </UserContext>
+    );
+}
+
+export function useAuthContext() {
+    const authState = use(UserContext);
+    if (authState === null) throw new Error("AuthContext not Found");
+    return authState;
+}
