efb6632d405162abbfd22cd19242d44ef57ea427
SeokGeunHo <kimc6555@gmail.com>
2025-07-19 13:08:32 +0900

    feat/be 토큰 재발급 로직, 쿠키 구현

--- backend/src/main/java/com/back/global/jwt/refreshtoken/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/back/global/jwt/refreshtoken/repository/RefreshTokenRepository.java
@@ -3,6 +3,9 @@ package com.back.global.jwt.refreshtoken.repository;
 import com.back.global.jwt.refreshtoken.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByMemberId(Long memberId);
 }
 
