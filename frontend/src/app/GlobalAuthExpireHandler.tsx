ce6da89057ba7101b28f94f35e4a11cb778235a6
asowjdan <asowjdan@naver.com>
2025-07-21 05:31:50 +0900

    로그인 만료를 구현하기 위한 컨트롤러 및 전역 레이아웃 지정

---
+++ frontend/src/app/GlobalAuthExpireHandler.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import ConfirmModal from "@/components/modal/ConfirmModal";
+import { setGlobalExpireHandler } from "@/lib/fetcher";
+
+export default function GlobalAuthExpireHandler() {
+    const [showExpireModal, setShowExpireModal] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        setGlobalExpireHandler(() => {
+            setShowExpireModal(true);
+        });
+    }, []);
+
+    const handleExpireConfirm = () => {
+        localStorage.clear();
+        setShowExpireModal(false);
+        window.location.href = "/members/login";
+    };
+
+    return (
+        <>
+            {showExpireModal && (
+                <ConfirmModal
+                    message={"로그인이 만료되었습니다.\n다시 로그인해주세요."}
+                    confirmText="확인"
+                    onConfirm={handleExpireConfirm}
+                    onCancel={handleExpireConfirm}
+                />
+            )}
+        </>
+    );
+} 
