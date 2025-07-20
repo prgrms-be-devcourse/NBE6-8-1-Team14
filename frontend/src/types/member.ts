e1d13e21fdd7dfeb4f10a2e5a5823b99f44f0a46
Gooraeng <birdyoon1998@gmail.com>
2025-07-20 11:43:57 +0900

    fix: 불필요한 내용 정리

--- frontend/src/types/member.ts
+++ frontend/src/types/member.ts
@@ -1,4 +1,3 @@
 import {components} from "@/lib/backend/api/schema";
 
 export type MemberLoginResponseDto = components["schemas"]["MemberLoginResponseDto"]
-export type MemberInfoResponseDto = components["schemas"]["MemberInfoResponseDto"]
