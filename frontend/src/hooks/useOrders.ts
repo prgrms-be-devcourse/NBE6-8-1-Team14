b0821224c63d3e88435908cebf204e6d3bc64522
Gooraeng <birdyoon1998@gmail.com>
2025-07-21 08:30:27 +0900

    fix: useOrder 복구

--- frontend/src/hooks/useOrders.ts
+++ frontend/src/hooks/useOrders.ts
@@ -1,63 +1,88 @@
-import { useEffect, useState } from "react";
-import { get, del } from "@/lib/fetcher";
-import type { Order } from "@/types/dev/order";
+import {useEffect, useState} from "react";
+import client from "@/lib/backend/client";
+import {useAuthContext} from "@/hooks/useAuth";
+import { Order } from "@/types/order";
+import { useRouter } from "next/navigation";
+import { ApiResponse } from "@/types/dev/auth";
+import {fromAdminSimpleOrders, fromMemberSimpleOrders} from "@/components/orders/convertOrderDtos";
 
-interface UseOrdersReturn {
+interface BaseOrderReturn {
     orders: Order[] | null;
-    loading: boolean;
-    error: string | null;
     cancelOrder: (id: number) => void;
     orderCanceled: boolean;
+    detailRequestUrl: (orderId: number) => string;
 }
 
-export function useOrders(): UseOrdersReturn {
-    const [orders, setOrders] = useState<Order[] | null>(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+export function useOrders(): BaseOrderReturn {
     const [orderCanceled, setOrderCanceled] = useState(false);
+    const { isLogin, getUserRole, loginMember } = useAuthContext();
+    const router = useRouter();
+    const [orders, setOrders] = useState<Order[] | null>(null);
+    const [baseUrl, setBaseUrl] = useState<string | null>(null);
 
-    const cancelOrder = (id: number) => {
-        del(`api/orders/${id}`)
-            .then((response) => {
-                if (response.error) {
-                    alert("주문취소에 실패했습니다.")
-                    return
-                }
-
-                setOrderCanceled(true);
-                alert("주문취소가 성공하였습니다")
-
-                if (orders == null) {
-                    return;
-                }
-
-                setOrders(orders.filter(order => order.id !== id));
-            })
+    const detailRequestUrl = (orderId: number) => {
+        return `${baseUrl}/${orderId}`;
     }
 
     useEffect(() => {
-        async function fetchOrders() {
-            setLoading(true);
-            const response = await get<Order[]>("test/orders/data/order.json");
-            if (response.error) {
-                setError(response.error);
-                setOrders(null);
-            } else {
-                const data = response.data
-                    ? response.data.toSorted((a, b) => {
-                        const aDate = a?.createdAt ?? '';
-                        const bDate = b?.createdAt ?? '';
-                        return bDate.localeCompare(aDate);
-                    })
-                    : []
+        // user가 없을 때 로그인 페이지로 리다이렉트
+        let requestUrl = null;
+        let converter = null;
 
-                setOrders(data);
-                setError(null);
-            }
-            setLoading(false);
+        const memberId = loginMember?.memberDto.id;
+
+        if (getUserRole() === "USER") {
+            requestUrl = `/api/orders/member/${memberId}`
+            converter = fromMemberSimpleOrders
+
+        } else if (getUserRole() === "ADMIN") {
+            requestUrl = `/api/admin/dashboard/${memberId}`
+            converter = fromAdminSimpleOrders
         }
-        fetchOrders();
-    }, []);
 
-    return { orders, loading, error, cancelOrder, orderCanceled };
+        if (!(requestUrl && converter)) {
+            return;
+        }
+
+        client.GET(requestUrl)
+            .then((res: ApiResponse<any>) => {
+                if (res.error) {
+                    return
+                } 
+                
+                const content = res.data?.content;
+                if (content && converter) {
+                    const orders : Order[] = converter(content);
+                    setOrders(orders);
+                    setBaseUrl(requestUrl);
+                }
+            }
+        )
+    }, [getUserRole, isLogin, loginMember?.memberDto.id, router]);
+
+    const cancelOrder = (orderId: number) => {
+        client.DELETE("/api/orders/{orderId}", {
+            params: {
+                path: {
+                    orderId
+                }
+            }
+        }).then((res) => {
+            if (res.error) {
+                alert("주문취소에 실패했습니다.")
+                return
+            }
+
+            setOrderCanceled(true);
+            alert("주문취소가 성공하였습니다")
+
+            if (!orders) {
+                return;
+            }
+
+            setOrders(orders.filter(order => order.id !== orderId));
+        })
+    }
+
+    return { orders, orderCanceled, cancelOrder, detailRequestUrl };
 }
