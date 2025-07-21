51a21774609fa463ed21555239e9325e74b01bf4
Gooraeng <birdyoon1998@gmail.com>
2025-07-22 00:06:04 +0900

    feat: useOrders 수정 - 로컬 스토리지 통해 상태 유지

--- frontend/src/hooks/useOrders.ts
+++ frontend/src/hooks/useOrders.ts
@@ -1,8 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuthContext } from "@/hooks/useAuth";
 import { get, del } from "@/lib/fetcher";
 import { Order } from "@/types/dev/order";
-import {fromAdminSimpleOrders, fromMemberSimpleOrders} from "@/components/orders/convertOrderDtos";
+import { fromAdminSimpleOrders, fromMemberSimpleOrders } from "@/components/orders/convertOrderDtos";
+import { useRouter } from "next/navigation";
 
 interface BaseOrderReturn {
     orders: Order[] | null;
@@ -11,6 +12,7 @@ interface BaseOrderReturn {
     cancelOrder: (id: number) => void;
     orderCanceled: boolean;
     detailRequestUrl: (orderId: number) => string;
+    getMemberIdFromLocalStorage: () => number;
 }
 
 function dummyFunction (data? : any) : Order[] {
@@ -18,30 +20,46 @@ function dummyFunction (data? : any) : Order[] {
 }
 
 export function useOrders(): BaseOrderReturn {
+    const router = useRouter();
     const [orderCanceled, setOrderCanceled] = useState(false);
     const { getUserRole, loginMember } = useAuthContext();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
     const [orders, setOrders] = useState<Order[] | null>(null);
     const [baseUrl, setBaseUrl] = useState<string | null>(null);
-    const requestedRef = useRef(false);
     const detailRequestUrl = (orderId: number) => {
         return `${baseUrl}/${orderId}`;
     }
+    const getMemberIdFromLocalStorage = () => {
+        let memberId = 0;
+        const loginState = localStorage.getItem("user-login-state");
+
+        if (loginState) {
+            try {
+                const parsed = JSON.parse(loginState);
+                memberId = parsed?.memberDto.id ?? 0;
+            } catch {
+
+            }
+        }
+
+        return memberId;
+    }
 
     useEffect(() => {
-        if (requestedRef.current) return;
-        requestedRef.current = true;
-
         async function fetchOrders() {
             // user가 없을 때 로그인 페이지로 리다이렉
             setLoading(true);
             const userRole = getUserRole();
 
             let requestUrl = '';
-            let converter: ((data: any) => Order[]) | null;
+            let converter: ((data: any) => Order[]) | null = dummyFunction;
 
-            const memberId = loginMember?.memberDto?.id;
+            if (userRole === "GUEST") {
+                return;
+            }
+
+            const memberId = getMemberIdFromLocalStorage();
 
             if (userRole === "USER") {
                 requestUrl = `/api/orders/member/${memberId}`
@@ -50,9 +68,6 @@ export function useOrders(): BaseOrderReturn {
             } else if (userRole === "ADMIN") {
                 requestUrl = `/api/admin/dashboard/${memberId}`
                 converter = fromAdminSimpleOrders
-            } else {
-                requestUrl = '/members/login'
-                converter = dummyFunction
             }
 
             const response = await get(requestUrl);
@@ -63,7 +78,7 @@ export function useOrders(): BaseOrderReturn {
                 let orders : Order[] = []
 
                 if (converter) {
-                    orders = converter(response.data);
+                    orders = converter(response.data.content);
                     orders = orders.toSorted((a, b) => b.id - a.id);
                 }
 
@@ -77,20 +92,19 @@ export function useOrders(): BaseOrderReturn {
     }, [getUserRole, loginMember?.memberDto.id]);
 
     const cancelOrder = (orderId: number) => {
-        del(`/api/orders/{orderId}`).then((res) => {
+        del(`/api/orders/${orderId}`).then((res) => {
             if (res.error) {
-                alert("주문취소에 실패했습니다.")
-                return
+                return;
             }
 
             setOrderCanceled(true);
-            alert("주문취소가 성공하였습니다")
 
             if (!orders) {
                 return;
             }
 
             setOrders(orders.filter(order => order.id !== orderId));
+            router.refresh();
         })
     }
 
@@ -100,6 +114,7 @@ export function useOrders(): BaseOrderReturn {
         orders,
         orderCanceled,
         cancelOrder,
-        detailRequestUrl
+        detailRequestUrl,
+        getMemberIdFromLocalStorage
     };
 }
